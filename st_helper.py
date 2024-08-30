dabfbb9a7a8026ff8c0527cdc0cdc0eeefaa4a17
Rahul Bhalley <0behinds_secant@icloud.com>
2024-08-30 21:34:13 +0530

    Added doc

--- st_helper.py
+++ st_helper.py
@@ -1,5 +1,3 @@
-import time
-import math
 import sys
 from glob import glob
 import shutil
@@ -11,141 +9,125 @@ import torch.optim as optim
 from imageio import imread, imwrite
 
 import utils
-from utils import *
-from vgg_pt import *
-from pyr_lap import *
+from vgg_pt import Vgg16_pt
+from pyr_lap import dec_lap_pyr, syn_lap_pyr
 from stylize_objectives import objective_class
 
-def style_transfer(stylized_im, content_im, style_path, output_path, scl, long_side, mask, content_weight=0., use_guidance=False, regions=0, coords=0, lr=2e-3):
+def style_transfer(stylized_im, content_im, style_path, output_path, scl, long_side, mask, 
+                   content_weight=0., use_guidance=False, regions=0, coords=0, lr=2e-3):
+    """
+    Perform style transfer on the given content image using the specified style.
 
+    Args:
+        stylized_im (torch.Tensor): Initial stylized image.
+        content_im (torch.Tensor): Content image.
+        style_path (str): Path to the style image or folder.
+        output_path (str): Path to save the output image.
+        scl (float): Scale factor for the output image.
+        long_side (int): Length of the long side of the image.
+        mask (torch.Tensor): Mask for region-based style transfer.
+        content_weight (float, optional): Weight for content loss. Defaults to 0.
+        use_guidance (bool, optional): Whether to use guided style transfer. Defaults to False.
+        regions (tuple, optional): Regions for region-based style transfer. Defaults to 0.
+        coords (torch.Tensor, optional): Coordinates for guided style transfer. Defaults to 0.
+        lr (float, optional): Learning rate for optimization. Defaults to 2e-3.
+
+    Returns:
+        tuple: A tuple containing:
+            - stylized_im (torch.Tensor): The final stylized image.
+            - ell (float): The final loss value.
+    """
     REPORT_INTERVAL = 100
     RESAMPLE_FREQ = 1
-    RESAMPLE_INCREASE_FREQ = 150
     MAX_ITER = 250
-    save_ind = 0
 
-    use_pyr=True
+    use_pyr = True
+    temp_name = f'./{output_path.split("/")[-1].split(".")[0]}_temp.png'
 
-    temp_name = './'+output_path.split('/')[-1].split('.')[0]+'_temp.png'
-
-    ### Keep track of current output image for GUI ###
+    # Keep track of current output image for GUI
     canvas = aug_canvas(stylized_im, scl, 0)
     imwrite(temp_name, canvas)
     shutil.move(temp_name, output_path)
 
-    #### Define feature extractor ###
+    # Define feature extractor
     cnn = utils.to_device(Vgg16_pt())
-
     phi = lambda x: cnn.forward(x)
-    phi2 = lambda x, y, z: cnn.forward_cat(x,z,samps=y,forward_func=cnn.forward)
+    phi2 = lambda x, y, z: cnn.forward_cat(x, z, samps=y, forward_func=cnn.forward)
 
-
-    #### Optimize over laplaccian pyramid instead of pixels directly ####
-
-
-    ### Define Optimizer ###
+    # Optimize over Laplacian pyramid instead of pixels directly
     if use_pyr:
-        s_pyr = dec_lap_pyr(stylized_im,5)
-        s_pyr = [Variable(li.data,requires_grad=True) for li in s_pyr]
+        s_pyr = dec_lap_pyr(stylized_im, 5)
+        s_pyr = [Variable(li.data, requires_grad=True) for li in s_pyr]
     else:
-        s_pyr = [Variable(stylized_im.data,requires_grad=True)]
+        s_pyr = [Variable(stylized_im.data, requires_grad=True)]
 
-    optimizer =  optim.RMSprop(s_pyr,lr=lr)
+    optimizer = optim.RMSprop(s_pyr, lr=lr)
 
-    ### Pre-Extract Content Features ###
+    # Pre-extract content features
     z_c = phi(content_im)
 
-    ### Pre-Extract Style Features from a Folder###
-    paths = glob(style_path+'*')[::3]
+    # Pre-extract style features from a folder
+    paths = glob(f'{style_path}*')[::3]
 
-    ### Create Objective Object ###
-    objective_wrapper = 0
+    # Create objective object
     objective_wrapper = objective_class(objective='remd_dp_g')
-    
 
     z_s_all = []
     for ri in range(len(regions[1])):
-        z_s, style_ims = load_style_folder(phi2, paths, regions,ri, n_samps=-1, subsamps=1000, scale=long_side, inner=5)
+        z_s, _ = load_style_folder(phi2, paths, regions, ri, n_samps=-1, subsamps=1000, scale=long_side, inner=5)
         z_s_all.append(z_s)
 
-    ### Extract guidance features if required ###
-    gs = np.array([0.])
+    # Extract guidance features if required
+    gs = torch.tensor([0.])
     if use_guidance:
-        gs = load_style_guidance(phi, style_path, coords[:,2:], scale=long_side)
+        gs = load_style_guidance(phi, style_path, coords[:, 2:], scale=long_side)
 
-
-    ### Randomly choose spatial locations to extract features from ###
-    if use_pyr:
-        stylized_im = syn_lap_pyr(s_pyr)
-    else:
-        stylized_im = s_pyr[0]
-
-    for ri in range(len(regions[0])):
-        
-
-        r_temp = regions[0][ri]
-        r_temp = torch.from_numpy(r_temp).unsqueeze(0).unsqueeze(0).contiguous()
-        r = F.upsample(r_temp,(stylized_im.size(3),stylized_im.size(2)),mode='bilinear')[0,0,:,:].numpy()        
-
-        if r.max()<0.1:
-            r = np.greater(r+1.,0.5)
-        else:
-            r = np.greater(r,0.5)
-
-        objective_wrapper.init_inds(z_c, z_s_all,r,ri)
+    # Initialize regions
+    stylized_im = syn_lap_pyr(s_pyr) if use_pyr else s_pyr[0]
+    for ri, r_temp in enumerate(regions[0]):
+        r_temp = F.interpolate(r_temp.unsqueeze(0).unsqueeze(0), 
+                               (stylized_im.size(3), stylized_im.size(2)), 
+                               mode='bilinear')[0, 0].numpy()
+        r = r_temp > 0.5 if r_temp.max() >= 0.1 else r_temp + 1 > 0.5
+        objective_wrapper.init_inds(z_c, z_s_all, r, ri)
 
     if use_guidance:
         objective_wrapper.init_g_inds(coords, stylized_im)
 
-
-
     for i in range(MAX_ITER):
-
-        ### zero out gradients and compute output image from pyramid ##
         optimizer.zero_grad()
-        if use_pyr:
-            stylized_im = syn_lap_pyr(s_pyr)
-        else:
-            stylized_im = s_pyr[0]
+        stylized_im = syn_lap_pyr(s_pyr) if use_pyr else s_pyr[0]
 
-        ## Dramatically Resample Large Set of Spatial Locations ##
-        if i==0 or i%(RESAMPLE_FREQ*10) == 0:
-            for ri in range(len(regions[0])):
-                
-                r_temp = regions[0][ri]
-                r_temp = torch.from_numpy(r_temp).unsqueeze(0).unsqueeze(0).contiguous()
-                r = F.upsample(r_temp,(stylized_im.size(3),stylized_im.size(2)),mode='bilinear')[0,0,:,:].numpy()        
+        # Resample spatial locations
+        if i == 0 or i % (RESAMPLE_FREQ * 10) == 0:
+            for ri, r_temp in enumerate(regions[0]):
+                r_temp = F.interpolate(r_temp.unsqueeze(0).unsqueeze(0), 
+                                       (stylized_im.size(3), stylized_im.size(2)), 
+                                       mode='bilinear')[0, 0].numpy()
+                r = r_temp > 0.5 if r_temp.max() >= 0.1 else r_temp + 1 > 0.5
+                objective_wrapper.init_inds(z_c, z_s_all, r, ri)
 
-                if r.max()<0.1:
-                    r = np.greater(r+1.,0.5)
-                else:
-                    r = np.greater(r,0.5)
-
-                objective_wrapper.init_inds(z_c, z_s_all,r,ri)
-
-        ## Subsample spatial locations to compute loss over ##
-        if i==0 or i%RESAMPLE_FREQ == 0:
+        if i == 0 or i % RESAMPLE_FREQ == 0:
             objective_wrapper.shuffle_feature_inds()
-        
-        ## Extract Features from Current Output
+
+        # Extract features from current output
         z_x = phi(stylized_im)
 
-        ## Compute Objective and take gradient step ##
-        ell = objective_wrapper.eval(z_x, z_c, z_s_all, gs, 0., content_weight=content_weight,moment_weight=1.0)
-
+        # Compute objective and take gradient step
+        ell = objective_wrapper.eval(z_x, z_c, z_s_all, gs, 0., content_weight=content_weight, moment_weight=1.0)
         ell.backward()
         optimizer.step()
-            
-        ## Periodically save output image for GUI ###
-        if (i+1)%10==0:
+
+        # Periodically save output image for GUI
+        if (i + 1) % 10 == 0:
             canvas = aug_canvas(stylized_im, scl, i)
             imwrite(temp_name, canvas)
             shutil.move(temp_name, output_path)
 
-        ### Periodically Report Loss and Save Current Image ###
-        if (i+1)%REPORT_INTERVAL == 0:
-            print('\t',(i+1),ell)
-            save_ind += 1
-
+        # Periodically report loss
+        if (i + 1) % REPORT_INTERVAL == 0:
+            print(f'\t{i + 1} {ell}')
 
     return stylized_im, ell
+
+# Additional helper functions (aug_canvas, load_style_folder, load_style_guidance) should be implemented here
